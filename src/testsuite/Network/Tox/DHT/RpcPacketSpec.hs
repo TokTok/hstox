86e837b83a1a7d621423ee4080d56313815d8bdd
iphydf <iphydf@users.noreply.github.com>
2016-07-14 16:07:19 +0100

    Remove unused imports; pretty-print msgpack-parser output.
    We now use the Text.Groom package to pretty-print the output from "show" when
    using the msgpack-parser tool. This helps a human understand the output better,
    especially when the output becomes larger.
    
    This commit also removes all unused imports and improves compatibility with
    older versions of GHC (7.6). We now use lists instead of vectors in the msgpack
    data model.

--- src/testsuite/Network/Tox/DHT/RpcPacketSpec.hs
+++ src/testsuite/Network/Tox/DHT/RpcPacketSpec.hs
@@ -1,14 +1,12 @@
-{-# OPTIONS_GHC -fno-warn-unused-imports #-}
 {-# LANGUAGE Trustworthy #-}
 module Network.Tox.DHT.RpcPacketSpec where
 
 import           Test.Hspec
-import           Test.QuickCheck
 
 import           Data.Proxy                (Proxy (..))
 import           Network.Tox.DHT.RpcPacket (RpcPacket)
-import qualified Network.Tox.DHT.RpcPacket as RpcPacket
 import           Network.Tox.EncodingSpec
+
 
 spec :: Spec
 spec = do
